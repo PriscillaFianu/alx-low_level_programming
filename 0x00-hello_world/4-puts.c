d2ecf2a37eec54043401634ddbe20a18de774c51
PriscillaFianu <priscillafianu80@gmail.com>
2023-04-21 15:15:29 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main -main entry
- * return : always (success)
+ *
+ * return:0
  */
 int main(void)
-{ puts("/"Programming is like building a multilingual puzzle");
+{
+       	puts("/"Programming is like building a multilingual puzzle");
 	return(0);
 }
