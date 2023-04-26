98f9cfff65f05e798fd4c508f6517bd4357a0df7
PriscillaFianu <priscillafianu80@gmail.com>
2023-04-26 16:42:05 -0700

    tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+	if (n == 113 || n == 120)
+	{
+	n++;
+	continue;
+	}
+	putchar (n);
+	n++;
+	}
+	putchar ('\n');
+	return (0);
+}
