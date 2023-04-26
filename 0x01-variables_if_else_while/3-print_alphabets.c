b3a35e356eedf2b265b3bf053837bfe9b70f2389
PriscillaFianu <priscillafianu80@gmail.com>
2023-04-26 16:00:40 -0700

    print all cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+	int r = 65;
+
+	while (n <= 122)
+	{
+	putchar (n);
+	}
+	putchar ('\n');
+	while (r = 90)
+	{
+	putchar (r);
+	}
+	putchar ('\r');
+	return (0);
+}
