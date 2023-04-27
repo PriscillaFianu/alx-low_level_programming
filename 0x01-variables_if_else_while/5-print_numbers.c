03d3cdc24c8786aa007889b1d50ab97edfa02eb5
PriscillaFianu <priscillafianu80@gmail.com>
2023-04-26 17:21:38 -0700

    tasks

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9;)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
